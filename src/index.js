6e5ec78e6caceaedbf2ed9ee2691c177215d5649
spope <spope851@gmail.com>
2022-11-10 15:33:24 -0700

    twitter feed component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { TwitterFeed } from './twitter-feed';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('twitter-feed-wrapper'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TwitterFeed />
   </React.StrictMode>
 );
 
